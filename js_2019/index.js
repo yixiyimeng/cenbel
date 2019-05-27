ac734c092b594208968451eda14b5edc0785baf0
zhangjing <zhangjing@huixiangtech.cn>
2019-05-27 14:25:19 +0800

    js事件

--- js_2019/index.js
+++ js_2019/index.js
@@ -1,10 +1,17 @@
-$('.dropdown-menu').hover(function() {
-			event.stopPropagation();
-			$('.header .dropdown-menu .dropdown-menu-wrap').slideToggle(300);
-		});
-		$(document).click(function(event) {
-			var _con = $('.header .dropdown-menu'); // 设置目标区域
-			if (!_con.is(event.target) && _con.has(event.target).length === 0) { // Mark 1
-				$('.header .dropdown-menu .dropdown-menu-wrap').slideUp(300);
-			}
-		});
+$(function() {
+	$('.header .dropdown-menu .dropdown-menu-wrap').width($('.header .dropdown-menu .dropdown-menu-wrap>div').length *
+		235 + ($('.header .dropdown-menu .dropdown-menu-wrap>div').length - 1) * 140);
+	$('.dropdown-menu').hover(function() {
+		event.stopPropagation();
+		$('.header .dropdown-menu .dropdown-menu-wrap').slideDown(300);
+	},function(){
+			$('.header .dropdown-menu .dropdown-menu-wrap').slideUp(300);
+	});
+	$(document).click(function(event) {
+		var _con = $('.header .dropdown-menu'); // 设置目标区域
+		if (!_con.is(event.target) && _con.has(event.target).length === 0) { // Mark 1
+			$('.header .dropdown-menu .dropdown-menu-wrap').slideUp(300);
+		}
+	});
+
+})
